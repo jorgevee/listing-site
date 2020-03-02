a27784ab1214f586386dc1e363281611ff462fd7
jorge <jorgevillegas77@gmail.com>
2020-03-01 19:55:55 -0800

    db collections

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -8,6 +8,12 @@ const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net`;
 
 export const connectDatabase = async () => {
   const client = await MongoClient.connect(url, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   });
+  const db = client.db("main");
+
+  return {
+    listings: db.collection("test_listings")
+  };
 }; 
