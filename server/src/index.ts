1e003e8cfb56406694b7cb135ee35c7530d455d0
jorge <jorgevillegas77@gmail.com>
2020-03-02 19:43:44 -0800

    express mongo query

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,13 +7,11 @@ const port = 9000;
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: () => ({ db })
-  });
 
   // ...
+
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
 };
 
 mount(express());
