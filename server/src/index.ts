9444029725e99065dd16e1414229115015d78b28
jorge <jorgevillegas77@gmail.com>
2020-03-02 19:41:28 -0800

    update express file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
 import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
+import { connectDatabase } from "./database";
 import { typeDefs, resolvers } from "./graphql";
 
 const port = 9000;
 
-const mount = (app: Application) => {
-  const server = new ApolloServer({ typeDefs, resolvers });
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  });
 
-  server.applyMiddleware({ app, path: "/api" });
-  app.listen(port);
-
-  console.log(`[app]: http://localhost:${port}`);
+  // ...
 };
 
 mount(express());
