de0e720877f3fb2c86bb09ac50ed7137422180ba
jorge <jorgevillegas77@gmail.com>
2020-03-04 21:51:53 -0800

    update mock db data

--- server/temp/seed.ts
+++ server/temp/seed.ts
@@ -1,2 +1,11 @@
 require("dotenv").config();
 
+const seed = async () => {
+  try {
+    console.log(`[seed] : running...`);
+  } catch (error) {
+    throw new Error("failed to seed database");
+  }
+};
+
+seed();
