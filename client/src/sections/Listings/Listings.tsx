2308f3e3eb1603dc605c91ead501c3f0bbdccfc8
jorge <jorgevillegas77@gmail.com>
2020-03-09 17:44:09 -0700

    update listing

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { server } from "../../lib/api";
 
-export const Listings = () => {
-  return <h2>TinyHouse Listings</h2>;
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  const fetchListings = () => {};
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+    </div>
+  );
 };
