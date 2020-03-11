1a79db98ac068e429af8761093a885143f74f462
jorge <jorgevillegas77@gmail.com>
2020-03-10 20:58:12 -0700

    Listing Update

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { server } from "../../lib/api";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  ListingsData
+} from "./types";
 
 const LISTINGS = `
   query Listings {
@@ -17,20 +22,42 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
 interface Props {
   title: string;
 }
 
 export const Listings = ({ title }: Props) => {
   const fetchListings = async () => {
-    const { data } = await server.fetch({ query: LISTINGS });
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
     console.log(data); // check the console to see the listings data from our GraphQL Request!
+  };
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "5d4507a9cf295034813b35c2" // hardcoded id variable,
+      }
+    });
+    console.log(data); // check the console to see the result of the mutation!
   };
 
   return (
     <div>
       <h2>{title}</h2>
       <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={deleteListing}>Delete a listing!</button>
     </div>
   );
 };
