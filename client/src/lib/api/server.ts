5c529f3ba3986f4401ef416c82dfdd0bc9317df7
jorge <jorgevillegas77@gmail.com>
2020-03-11 20:22:01 -0700

    update api

--- client/src/lib/api/server.ts
+++ client/src/lib/api/server.ts
@@ -4,7 +4,9 @@ interface Body<TVariables> {
 }
 
 export const server = {
-  fetch: async <TData = any, TVariables = any>(body: Body<TVariables>) => {
+  fetch: async <TData = any, TVariables = any>(
+    body: Body<TVariables>
+  ) => {
     const res = await fetch("/api", {
       method: "POST",
       headers: {
