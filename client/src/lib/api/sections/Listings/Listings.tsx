24bc331c894f6f1f7932800501e7da78c38ac9f7
jorge <jorgevillegas77@gmail.com>
2020-03-12 21:19:35 -0700

    Added react hooks

--- client/src/lib/api/sections/Listings/Listings.tsx
+++ client/src/lib/api/sections/Listings/Listings.tsx
@@ -1,19 +1,67 @@
-xport const Listings = ({ title }: Props) => {
+import React, { useState } from "react";
+import { server } from "../../lib/api";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  Listing,
+  ListingsData
+} from "./types";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
   const [listings, setListings] = useState<Listing[] | null>(null);
 
   const fetchListings = async () => {
-    const { data } = await server.fetch<ListingsData>({
-      query: LISTINGS
-    });
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
     setListings(data.listings);
   };
 
-  // ...
+  const deleteListing = async (id: string) => {
+    await server.fetch<DeleteListingData, DeleteListingVariables>({
+      query: DELETE_LISTING,
+      variables: {
+        id
+      }
+    });
+    fetchListings();
+  };
 
   const listingsList = listings ? (
     <ul>
       {listings.map(listing => {
-        return <li key={listing.id}>{listing.title}</li>;
+        return (
+          <li key={listing.id}>
+            {listing.title}{" "}
+            <button onClick={() => deleteListing(listing.id)}>Delete</button>
+          </li>
+        );
       })}
     </ul>
   ) : null;
@@ -23,7 +71,6 @@ xport const Listings = ({ title }: Props) => {
       <h2>{title}</h2>
       {listingsList}
       <button onClick={fetchListings}>Query Listings!</button>
-      <button onClick={deleteListing}>Delete a listing!</button>
     </div>
   );
 };
