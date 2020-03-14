a71b5a1a675899ba7c8b00420c9d8ca34c6b4a56
jorge <jorgevillegas77@gmail.com>
2020-03-13 20:32:40 -0700

    use query

---
+++ client/src/lib/api/useQuery.ts
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import { server } from "./server";
+
+interface State<TData> {
+  data: TData | null;
+}
+
+export const useQuery = <TData = any>(query: string) => {
+  const [state, setState] = useState<State<TData>>({
+    data: null
+  });
+};
