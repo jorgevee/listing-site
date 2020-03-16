3ad32f2d9d0887798f9488d321803da42d0b8986
jorge <jorgevillegas77@gmail.com>
2020-03-15 19:15:59 -0700

    update Listing section

---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,26 @@
+export const Listings = ({ title }: Props) => {
+  const { data, refetch } = useQuery<ListingsData>(LISTINGS);
+
+  const deleteListing = async (id: string) => {
+    await server.fetch<DeleteListingData, DeleteListingVariables>({
+      query: DELETE_LISTING,
+      variables: {
+        id
+      }
+    });
+
+    refetch();
+  };
+
+  const listings = data ? data.listings : null;
+
+  const listingsList = listings ? (
+    <ul>
+      {listings.map(listing => {
+        return (
+          <li key={listing.id}>
+            {listing.title}{" "}
+            <button onClick={() => deleteListing(listing.id)}>Delete</button>
+          </li>
+        );
+      })}
