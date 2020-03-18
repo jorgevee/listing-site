b50b440aac227a20a51ac650b31f4ba28ef184a8
jorge <jorgevillegas77@gmail.com>
2020-03-17 20:19:43 -0700

    mutaion update

--- client/src/lib/api/useMutaion.ts
+++ client/src/lib/api/useMutaion.ts
@@ -1,2 +1,10 @@
 import { useState } from "react";
 import { server } from "./server";
+
+interface State<TData> {
+  data: TData | null;
+  loading: boolean;
+  error: boolean;
+}
+
+export const useMutation = <TData = any, TVariables = any>(query: string) => {};
